167e66498c2c8e546b3289e80e2f8b1819a2e287
tjgraham <77311481+tjgraham@users.noreply.github.com>
2021-04-13 21:56:10 +1000

    Add tests for time-related functions

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,27 @@ using SafeBluesCampusSimulation
 using Test
 
 @testset "SafeBluesCampusSimulation.jl" begin
-    # Write your tests here.
+    @testset "hour" begin
+        @test SafeBluesCampusSimulation.hour(1) == 1    # 1 -> 12am -- 1am
+        @test SafeBluesCampusSimulation.hour(25) == 1   # 25 -> 12am -- 1am
+        @test SafeBluesCampusSimulation.hour(50) == 2   # 50 -> 1am -- 2am
+        @test SafeBluesCampusSimulation.hour(100) == 4  # 100 -> 3am -- 4am
+        @test SafeBluesCampusSimulation.hour(150) == 6  # 150 -> 5am -- 6am
+    end
+
+    @testset "day" begin
+        @test SafeBluesCampusSimulation.day(1) == 1    # 1 -> Monday
+        @test SafeBluesCampusSimulation.day(25) == 2   # 25 -> Tuesday
+        @test SafeBluesCampusSimulation.day(50) == 3   # 50 -> Wednesday
+        @test SafeBluesCampusSimulation.day(100) == 5  # 100 -> Friday
+        @test SafeBluesCampusSimulation.day(150) == 7  # 150 -> Sunday
+    end
+
+    @testset "is_weekend" begin
+        @test SafeBluesCampusSimulation.is_weekend(25) == false   # 25 -> Tuesday
+        @test SafeBluesCampusSimulation.is_weekend(50) == false   # 50 -> Wednesday
+        @test SafeBluesCampusSimulation.is_weekend(100) == false  # 100 -> Friday
+        @test SafeBluesCampusSimulation.is_weekend(125) == true   # 125 -> Saturday
+        @test SafeBluesCampusSimulation.is_weekend(150) == true   # 150 -> Sunday
+    end
 end
